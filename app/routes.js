087f8ed79eb25c838c70fd877374fd3b96bf0e2e
ismail-co <ismail.ismail@cabinetoffice.gov.uk>
2023-03-13 12:33:49 +0000

    v1

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,17 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+router.post("/check-eligible", function (req, res) {
+  var right = req.session.data['righttoremain'];
+  var member = req.session.data['memberships'];
+  var lang = req.session.data['language'];
+  var ess1 = req.session.data['essential1'];
+  var ess2 = req.session.data['essential2'];
+
+  if (right === "yes" && member === "yes" && lang === "yes" && ess1 === "yes" && ess2 === "yes") {
+    res.redirect('task-list')
+  } else {
+    res.redirect('not-eligible')
+  }
+})
